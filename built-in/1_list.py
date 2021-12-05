29cbea08b8e05ab9e98e320f186b1e5b68035f1e
Jay Prakash Thakur <jayprakashthakursnr@gmail.com>
2021-12-04 18:31:55 -0600

    added builtin list

---
+++ built-in/1_list.py
@@ -0,0 +1,35 @@
+hello = [1, 20, 'Jay', 10.0]
+
+print(type(hello))
+print(hello)
+
+# # indexing 
+# print(hello[0])
+# print(hello[3])
+# print(hello[-1])
+
+# print(hello[:2])
+
+# # list buildin methods
+# # adding elements to list
+
+# hello.append("Thakur")
+# print(hello)
+# hello.insert(1, '30')
+# print(hello)
+# hello.extend([90, 100])
+# print(hello)
+
+# # Deleting elements
+# e = hello.pop()
+# print(e)
+
+# hello.remove(90)
+# print(hello)
+# hello.clear()
+# print(hello)
+
+# hello.sort()
+print(hello.count(10))
+
+print(hello.index('Jay'))
